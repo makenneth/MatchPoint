eec655f6c495b59f9ab827e8c19a88b283df1906
makenneth <kenneth.km.ma@gmail.com>
2018-01-29 22:38:36 -0800

    Refactored player api handler; added controller

--- webapp/app/api/players.js
+++ webapp/app/api/players.js
@@ -1,122 +1,25 @@
 import express from "express";
-import Club from "../models/club";
-import Player from "../models/player";
-import { csrfProtection, clubMethods, client } from "../helpers/appModules";
-import PlayerValidation from "../validations/player";
+import { csrfProtection } from "../helpers/appModules";
 
 const router = express.Router();
-router.route("/players").get((req, res, next) => {
-  const clubId = req.club.id;
+router.route("/players")
+  .get(Controller.getPlayers)
+  .post(csrfProtection, Controller.createPlayer);
 
-  client.get(`players:${clubId}`, (err, reply) => {
-    if (err) throw err;
-    if (!reply) {
-      Player.findPlayers(clubId)
-        .then((players) => {
-          res.status(200).send({ players });
-          try {
-            const json = JSON.stringify(players);
-            client.setex(`players:${clubId}`, 24*60*60, json);
-          } catch (e) {
-            next({ code: 500, messgae: e });
-          }
-        }).catch(err => next({ code: 500, message: err }));
-    } else {
-      console.log('cache hit');
-      try {
-        const players = JSON.parse(reply);
-        res.status(200).send({ players });
-      } catch (_e) {
-        next({ code: 500, message: 'Failed to parse redis data.' });
-      }
-    }
-  });
-})
-.post(csrfProtection, (req, res, next) => {
-  const clubId = req.club.id;
-  const data = req.body.player;
-  const [hasError, err] = PlayerValidation(data);
-  if (hasError) {
-    return next({ code: 422, message: err });
-  }
-  console.log(req.club);
-  Player.createPlayer(clubId, data)
-    .then(
-      async (playerId) => {
-        try {
-          const player = await Player.find(clubId, playerId);
-          client.del(`players:${clubId}`);
-          res.status(200).send({ player });
-        } catch (e) {
-          console.log(e);
-          next({ code: 500, message: e });
-        }
-      }
-    ).catch(err => {
-      console.log(err);
-      next({ code: 500, message: err })
-    });
-});
-
-router.route("/players/active").get((req, res, next) => {
-  // not tested...
-  const clubId = req.club.id;
-  Player.getMostActivePlayers(clubId)
-    .then((data) => {
-      res.status(200).send({ players: data });
-    }).catch(err => next({ code: 500, message: err }));
-});
+// router.route("/players/active").get((req, res, next) => {
+//   // not tested...
+//   const clubId = req.club.id;
+//   Player.getMostActivePlayers(clubId)
+//     .then((data) => {
+//       res.status(200).send({ players: data });
+//     }).catch(err => next({ code: 500, message: err }));
+// });
 
 router.route("/players/:id")
-  .delete(csrfProtection, (req, res, next) => {
-    const clubId = req.club.id;
-    const playerId = req.params.id;
-    Player.removePlayer(clubId, playerId)
-      .then(
-        () => {
-          client.del(`players:${clubId}`);
-          res.status(200).send({ playerId });
-        },
-        (err) => {
-          if (err.player) {
-            return next({ code: 422, message: err });
-          }
-          throw err;
-        }
-      ).catch(err => next({ code: 500, message: err }));
-  })
-  .patch(csrfProtection, (req, res, next) => {
-    const clubId = req.club.id;
-    const id = req.params.id;
-    const player = req.body.player;
+  .delete(csrfProtection, Controller.deletePlayer)
+  .patch(csrfProtection, Controller.updatePlayer);
 
-    Player.updatePlayer(clubId, id, player)
-      .then(
-        async () => {
-          try {
-            const player = await Player.find(clubId, id);
-            client.del(`players:${clubId}`);
-            res.status(200).send({ player });
-          } catch (e) {
-            console.log(e);
-            next({ code: 500, message: e });
-          }
-        },
-        (err) => {
-          if (err.player) {
-            return next({ code: 422, message: err });
-          }
-          throw err;
-        }).catch(err => next({ code: 500, message: err }));
-  });
-
-router.route("/players/promotion").get(async (req, res, next) => {
-  try {
-    const promoted = await Player.findPromotedPlayers(req.club.id);
-    res.status(200).send({ promoted });
-  } catch (e) {
-    return next({ code: 500, message, e });
-  }
-});
+router.route("/players/promotion")
+  .get(Controller.getPromotedPlayers);
 
 export default router;
