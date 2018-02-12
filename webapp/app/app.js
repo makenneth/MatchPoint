096ece375249662191e06d997cd8c82e96ff7282
makenneth <kenneth.km.ma@gmail.com>
2018-02-11 18:19:33 -0800

    Remove unncessary production check

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('dotenv').config()
 import path from "path";
 import cookieParser from "cookie-parser";
 import express from "express";
