8009fb0e850be6fe5742724af410f58437fc3297
makenneth <kenneth.km.ma@gmail.com>
2018-02-17 17:37:28 -0800

    Added order by to further improve query

--- webapp/app/models/roundrobin.js
+++ webapp/app/models/roundrobin.js
@@ -119,6 +119,7 @@ class RoundRobin {
           ON phs1.player_id = phs2.player_id AND
              phs1.change_date = phs2.max_date AND
              phs1.club_id = phs2.club_id
+          ORDER BY phs1.id DESC
           LIMIT 1
         ) AS ph1
         ON p.id = ph1.player_id AND r.finalized = 0
@@ -128,6 +129,7 @@ class RoundRobin {
           INNER JOIN roundrobins AS r
           ON r.id = ph.roundrobin_id
           WHERE r.short_id = ?
+          ORDER BY ph.id DESC
           LIMIT 1
         ) AS ph2
         ON p.id = ph2.player_id AND r.finalized = 1
