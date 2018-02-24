95587376390a1a807e9552d356cd76ded4a249d3
makenneth <kenneth.km.ma@gmail.com>
2018-02-23 19:09:52 -0800

    Move google api key to env

--- webapp/app/helpers/googleApi.js
+++ webapp/app/helpers/googleApi.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 import request from 'request-promise';
 import { client } from "../helpers/appModules";
-
 export async function getPredictions(query) {
-  const apiKey = 'AIzaSyD6ANrl7aamAgPdRf9d0OsDsnlbMXos7xg';
+  const apiKey = process.env.GOOGLE_SEARCH_API_KEY;
   const googleApiUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
   const response = await request(`${googleApiUrl}?types=address&key=${apiKey}&input=${query}`);
   const data = JSON.parse(response);
@@ -18,7 +18,7 @@ export async function getPredictions(query) {
 }
 
 export async function getGeoCode(address) {
-  const apiKey = 'AIzaSyDgjXJPSkK2rnYCMuVpxUpVc9WmrVixbaM';
+  const apiKey = process.env.GEOCODE_API_KEY;
   const googleApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
 
   const response = await request(`${googleApiUrl}?key=${apiKey}&address=${address}`);
